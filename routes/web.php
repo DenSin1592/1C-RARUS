2b180bc535ab085086ac3a05784ed282e09cb0d3
DenSin1592 <wanderer1592@gmail.com>
2020-03-10 21:36:46 +0300

    Настроил авторизацию и роутинг

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
+
+Route::get('/home', function () {
+    return redirect('/main');
+});
+
+Auth::routes();
+
+Route::get('/main', 'MainController@index');
